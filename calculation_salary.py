b8057cade2dcd9b0fbbbfa270e5e7517acd7ddbc
Your USERNAME <Your EMAIL>
2019-02-09 16:24:36 +0300

    Исправил условия

--- calculation_salary.py
+++ calculation_salary.py
@@ -1,10 +1,10 @@
 
 def predict_rub_salary(payment_from, payment_to):
-    if not (payment_from is None or payment_to is None):
+    if payment_from and payment_to:
         salary = (payment_from + payment_to) / 2
-    elif payment_from is not None:
+    elif payment_from:
         salary = payment_from * 1.2
-    elif payment_to is not None:
+    elif payment_to:
         salary = payment_to * 0.8
     else:
         salary = None
